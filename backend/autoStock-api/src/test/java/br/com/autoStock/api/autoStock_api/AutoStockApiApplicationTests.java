3eb38378181ba2a557e47a4e6155b135d3bf603a
Cláudio Vinícius <claudiodev175@gmail.com>
2026-03-23 13:38:42 -0300

    Inicialização do backend

---
+++ backend/autoStock-api/src/test/java/br/com/autoStock/api/autoStock_api/AutoStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autoStock.api.autoStock_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
