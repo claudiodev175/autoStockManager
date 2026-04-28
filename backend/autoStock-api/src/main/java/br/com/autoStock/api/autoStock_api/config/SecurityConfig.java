6347f136810294b82926a0741ad4231a8a0456de
Cláudio Vinícius <claudiodev175@gmail.com>
2026-04-28 20:02:00 -0300

    Finalização da autenticação JWT

--- backend/autoStock-api/src/main/java/br/com/autoStock/api/autoStock_api/config/SecurityConfig.java
+++ backend/autoStock-api/src/main/java/br/com/autoStock/api/autoStock_api/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                         .requestMatchers("/api/produtos/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(basic -> basic.disable())
